7ea3149c25baecacbed29510d60c88e4cdd4b1e2
devdasgod1-sudo <devdasgod1@gmail.com>
2026-02-15 00:19:46 +0545

    ooo

--- src/app/actions/getBannerData.ts
+++ src/app/actions/getBannerData.ts
@@ -4,8 +4,8 @@ import RemoteServices from '../api/remoteservice';
 
 export async function getBannerData(slug: string) {
   try {
-    const res = await RemoteServices.getBannerSlug(slug);
-    return res.data?.[0] || null;
+    const res = await RemoteServices.getBannerBySlug(slug);
+    return res.data|| null;
   } catch (error) {
     console.error(`Failed to fetch banner data for slug: ${slug}`, error);
     return null;
